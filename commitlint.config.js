24faa1262af62a5778d58428c4452e52bf26a9ba
1190201924 <zhuzhu8_4@163.com>
2023-01-16 11:54:14 +0800

    0.0.0

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+// commitlint.config.js
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "type-enum": [2, "always", ["feat", "fix", "revert"]],
+      "subject-max-length": [1, "always", 30],
+    },
+  };
